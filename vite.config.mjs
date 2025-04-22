812e3c2ed8c9d91cbdeb8bf68ba090057502f180
Amirali <amirali.kadenov@gmail.com>
2025-04-22 18:27:34 +0500

    refactor: vite config - remove redundant

--- vite.config.mjs
+++ vite.config.mjs
@@ -5,16 +5,13 @@ import path from 'path'
 export default defineConfig({
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
       '@styles': path.resolve(__dirname, 'src/app/styles'),
     },
-    extensions: ['.mjs', '.js', '.json'],
   },
 
   plugins: [
     tsconfigPaths({
       loose: true,
-      extensions: ['.mjs', '.js', '.json'],
     }),
   ],
 })
