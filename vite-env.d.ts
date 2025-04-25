19b80228f6e6d2dc768c228e1f68417391a90deb
Amirali <amirali.kadenov@gmail.com>
2025-04-25 16:09:40 +0500

    add: env variables - proxy url, rates api

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -3,4 +3,6 @@
 interface ImportMetaEnv {
     readonly VITE_LOGIN_EMAIL: string
     readonly VITE_LOGIN_PASSWORD: string
+    readonly VITE_PROXY_URL: string
+    readonly VITE_EXCHANGE_RATES_API: string
 }
